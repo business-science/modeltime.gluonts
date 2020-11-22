2186081a955a277f4129e24192ee783c61389441
Matt Dancho <mdancho@gmail.com>
2020-11-22 13:29:17 -0500

    Detecting conda

--- R/00_global_variables.R
+++ R/00_global_variables.R
@@ -1,5 +1,5 @@
 utils::globalVariables(
     names = c(
-        "new_data", "object", ".actual", ".fitted", "fail_check", ".row_id", "value", "id"
+        "new_data", "object", ".actual", ".fitted", "fail_check", ".row_id", "value", "id", "python"
     )
 )
--- R/zzz.R
+++ R/zzz.R
@@ -4,12 +4,25 @@
 #' @import modeltime
 #' @importFrom reticulate py
 
-# PACKAGE ENVIRONMENT ----
+# PACKAGE ENVIRONMENT SETUP ----
+
+detect_conda_env <- function() {
+
+    tryCatch({
+        ret <- reticulate::conda_list() %>%
+            dplyr::filter(stringr::str_detect(python, pkg.env$env_name))
+    }, error = function(e) {
+        ret <- NULL
+    })
+
+    return(ret)
+
+}
+
 pkg.env            <- new.env()
 pkg.env$env_name   <- "r-gluonts"
 pkg.env$activated  <- FALSE
-pkg.env$conda_envs <- reticulate::conda_list() %>%
-    dplyr::filter(stringr::str_detect(python, pkg.env$env_name))
+pkg.env$conda_envs <- detect_conda_env()
 
 
 
@@ -48,18 +61,18 @@ np      <- NULL
 
 # UTILITIES ----
 
-install_on_travis <- function() {
-    if (!identical(Sys.getenv("TRAVIS"), "true")) {
-        install_gluonts()
-    }
-}
-
 activate_gluonts <- function() {
 
     conda_envs_found <- nrow(pkg.env$conda_envs)
 
-    if (conda_envs_found == 0) {
-        message("Please use 'install_gluonts()' to set up the a conda environment named 'r-gluonts' before using modeltime.gluonts.")
+    if (is.null(conda_envs_found)) {
+        # No conda???
+        message("Error detecting Conda Python Environments with `reticulate::conda_list()`. Conda is required for 'modeltime.gluonts'. Try using `reticulate::conda_install()`.")
+        pkg.env$activated <- FALSE
+
+    }else if (conda_envs_found == 0) {
+        message("Please use 'install_gluonts()' to set up the a conda environment named 'r-gluonts' before using modeltime.gluonts. You only need to do this once.")
+        pkg.env$activated <- FALSE
     } else if (conda_envs_found > 1) {
         message("Multiple 'r-gluonts' python environments found.")
         print(pkg.env$conda_envs)
