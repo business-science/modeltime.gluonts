02285aa287df80d20276f3227be66c6ec7bab23f
flrs <9593883+flrs@users.noreply.github.com>
2021-02-23 19:56:20 -0800

    Add dials functions for some nbeats tuning parameters

---
+++ R/dials-nbeats_params.R
@@ -0,0 +1,65 @@
+#' Tuning Parameters for NBEATS Models
+#'
+#' @inheritParams dials::learn_rate
+#' @param values A character string of possible values.
+#'
+#' @details
+#' The main parameters for NBEATS models are:
+#'
+#' - `num_stacks`: The number of stacks the network should contain.
+#' - `num_batches_per_epoch`: Number of batches at each epoch.
+#' - `learn_rate_decay_factor`: Factor by which to decrease the learning rate.
+#' - `scale`: Scales numeric data by `id' group using mean = 0, standard deviation = 1 transformation.
+#'
+#' @name nbeats_params
+
+#' @export
+#' @rdname nbeats_params
+num_stacks <- function(range = c(2, 100), trans = NULL) {
+    dials::new_quant_param(
+        type      = "integer",
+        range     = "range",
+        inclusive = c(TRUE, TRUE),
+        trans     = trans,
+        label     = c(num_stacks = "Number of Stacks"),
+        finalize  = NULL
+    )
+}
+
+#' @export
+#' @rdname nbeats_params
+num_batches_per_epoch <- function(range = c(2, 100), trans = NULL) {
+    dials::new_quant_param(
+        type      = "integer",
+        range     = "range",
+        inclusive = c(TRUE, TRUE),
+        trans     = trans,
+        label     = c(num_stacks = "Number of Batches at Each Epoch"),
+        finalize  = NULL
+    )
+}
+
+#' @export
+#' @rdname nbeats_params
+learn_rate_decay_factor <- function(range = c(0.0, 1.0), trans = NULL) {
+    dials::new_quant_param(
+        type      = "double",
+        range     = "range",
+        inclusive = c(TRUE, TRUE),
+        trans     = trans,
+        label     = c(num_stacks = "Learning Rate Decay Factor"),
+        finalize  = NULL
+    )
+}
+
+#' @export
+#' @rdname nbeats_params
+scale <- function(values = c(TRUE, FALSE)) {
+    dials::new_qual_param(
+        type     = c("logical"),
+        values   = values,
+        default  = FALSE,
+        label    = c(scale = "Scale Numeric Data"),
+        finalize = NULL
+    )
+}
